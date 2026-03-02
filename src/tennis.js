e57589730c9f451de9108b8dc172da2c7b784327
BigSmoke <cesarcadiz26@gmail.com>
2026-03-02 14:23:24 -0400

    Caso Deuce implementado

--- src/tennis.js
+++ src/tennis.js
@@ -1,19 +1,35 @@
 class Tennis {
     constructor() {
         this.player1Points = 0;
+        this.player2Points = 0;
     }
     score(){
         let player1Score = "Love";
-        if(this.player1Points == 1){
-            player1Score= "15"
+        let player2Score = "Love";
+        if (this.player1Points == 1) player1Score = "15";
+        if (this.player1Points == 2) player1Score = "30";
+        if (this.player1Points == 3) player1Score = "40";
+        if (this.player2Points == 1) player2Score = "15";
+        if (this.player2Points == 2) player2Score = "30";
+        if (this.player2Points == 3) player2Score = "40";
+    
+        const p1 = this.player1Points;
+        const p2 = this.player2Points;
+        if (p1 >= 3 && p2 >= 3) {
+        if (p1 === p2) {
+        return "Deuce";
         }
-        if(this.player1Points == 2){
-            player1Score= "30"
-        }
-        if(this.player1Points == 3){
-            player1Score= "40"
-        }
-        return player1Score+" - Love"
+
+        const diff = p1 - p2;
+        if (diff === 1) return "Advantage Player 1";
+        if (diff === -1) return "Advantage Player 2";
+
+        if (diff >= 2) return "Game for Player 1";
+        if (diff <= -2) return "Game for Player 2";
+    }
+              
+        return "Advantage player 1"
+
     }
 
     player1Scores(){
