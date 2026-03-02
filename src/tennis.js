96f93bb72724e3abf568a090a5ecca5d9e9388c9
BigSmoke <cesarcadiz26@gmail.com>
2026-03-02 14:28:36 -0400

    Pruebas de jugador 1 ganador completada

--- src/tennis.js
+++ src/tennis.js
@@ -1,40 +1,54 @@
 class Tennis {
-    constructor() {
-        this.player1Points = 0;
-        this.player2Points = 0;
-    }
-    score(){
-        let player1Score = "Love";
-        let player2Score = "Love";
-        if (this.player1Points == 1) player1Score = "15";
-        if (this.player1Points == 2) player1Score = "30";
-        if (this.player1Points == 3) player1Score = "40";
-        if (this.player2Points == 1) player2Score = "15";
-        if (this.player2Points == 2) player2Score = "30";
-        if (this.player2Points == 3) player2Score = "40";
+  constructor() {
+    this.player1Points = 0;
+    this.player2Points = 0;
+  }
+
+  score() {
+    let player1Score = "Love";
+    let player2Score = "Love";
+
     
-        const p1 = this.player1Points;
-        const p2 = this.player2Points;
-        if (p1 >= 3 && p2 >= 3) {
-        if (p1 === p2) {
+    if (this.player1Points == 1) player1Score = "15";
+    if (this.player1Points == 2) player1Score = "30";
+    if (this.player1Points == 3) player1Score = "40";
+
+    if (this.player2Points == 1) player2Score = "15";
+    if (this.player2Points == 2) player2Score = "30";
+    if (this.player2Points == 3) player2Score = "40";
+
+    const p1 = this.player1Points;
+    const p2 = this.player2Points;
+
+    
+    if (p1 >= 3 && p2 >= 3) {
+      if (p1 === p2) {
         return "Deuce";
-        }
+      }
 
-        const diff = p1 - p2;
-        if (diff === 1) return "Advantage Player 1";
-        if (diff === -1) return "Advantage Player 2";
+      const diff = p1 - p2;
+      if (diff === 1) return "Advantage Player 1";
+      if (diff === -1) return "Advantage Player 2";
 
-        if (diff >= 2) return "Game for Player 1";
-        if (diff <= -2) return "Game for Player 2";
-    }
-              
-        return "Advantage player 1"
-
+      if (diff >= 2) return "Game for Player 1";
+      if (diff <= -2) return "Game for Player 2";
     }
 
-    player1Scores(){
-        this.player1Points++;
-    }
+   
+    if (p1 === 4) return "Game for Player 1";
+    if (p2 === 4) return "Game for Player 2";
+
+    
+    return `${player1Score} - ${player2Score}`;
+  }
+
+  player1Scores() {
+    this.player1Points++;
+  }
+
+  player2Scores() {
+    this.player2Points++;
+  }
 }
 
 export default Tennis;
